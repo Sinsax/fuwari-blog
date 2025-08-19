3baa5c09b03803fceb6219eda8054e60f49daab1
Sinsax <34959437+Sinsax@users.noreply.github.com>
2025-08-19 22:50:57 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://sinsax.github.io",
+	base: "/Sinsapost",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
