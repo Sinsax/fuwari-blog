555233a5e8d443e6f34a14cdc79a83c0e171c0b9
Sinsax <34959437+Sinsax@users.noreply.github.com>
2025-08-19 22:56:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sinsax.github.io",
+	site: "https://sinsax.github.io/",
 	base: "/Sinsapost",
 	trailingSlash: "always",
 	integrations: [
