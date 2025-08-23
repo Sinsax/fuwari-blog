ab86fa332f8542226bcd19ee502d7b202b1e9caf
Sinsax <34959437+Sinsax@users.noreply.github.com>
2025-08-23 19:40:59 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sinsax.github.io/",
+	site: "http://blog.sinsa.icu/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
