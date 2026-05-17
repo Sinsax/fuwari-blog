cad8f45d139864379bcf11c004d6f6e3dea5cfae
Sinsax <2498349501@qq.com>
2026-05-18 04:11:46 +0800

    Add new avatar image for profile and update configuration

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/lize.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/mutsumi.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "SInsa",
 	bio: "",
 	links: [
