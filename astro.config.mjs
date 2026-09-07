2b80aca2f3e437d4afe9a4a9ebd310d323e9a8f4
Sinsax <2498349501@qq.com>
2026-09-07 08:04:51 +0800

    更新站点配置中的域名为 blog.sinsa.cc.cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.sinsa.icu/",
+	site: "https://blog.sinsa.cc.cd/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
